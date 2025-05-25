e055ba5276946877b35ab865b0cf546ef011bf00
Alex Duban Galvis Guarnizo <alex.galvis@sofka.local>
2025-05-25 08:34:32 -0500

    se agrega report junit

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,9 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './src/tests/e2e',
-  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }],
+             ['junit', { outputFile: 'test-results/e2e-junit-results.xml' }]
+            ],
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
